004d86ba2b77a2aa4d860f787ca941832e73117b
Omer Besent <omer.besent@hotmail.com>
2023-03-17 01:16:09 +0300

    ContactInformationsController için Add,Delete methodları eklendi

--- PhoneBook.WebAPI/Controllers/ContactInformationsController.cs
+++ PhoneBook.WebAPI/Controllers/ContactInformationsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneBook.Business.Abstract;
+using PhoneBook.Entities.Concrete;
 
 namespace PhoneBook.WebAPI.Controllers
 {
@@ -18,6 +19,22 @@ namespace PhoneBook.WebAPI.Controllers
         public IActionResult GetByPersonId(int personId)
         {
             var result = _contactInformationService.GetListByPersonId(personId);
+
+            return Ok(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(ContactInformation contactInformation)
+        {
+            var result = _contactInformationService.Add(contactInformation);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int contactInformationId)
+        {
+            var result = _contactInformationService.Delete(contactInformationId);
 
             return Ok(result);
         }
