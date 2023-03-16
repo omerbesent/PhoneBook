4ce829dda365e8f96e554c5e63d0eeaf8dc21b03
Omer Besent <omer.besent@hotmail.com>
2023-03-17 01:10:47 +0300

    ContactInformationsController oluşturulup kişi idsine göre listeeme yapıldı

---
+++ PhoneBook.WebAPI/Controllers/ContactInformationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Business.Abstract;
+
+namespace PhoneBook.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactInformationsController : ControllerBase
+    { 
+        private readonly IContactInformationService _contactInformationService;
+
+        public ContactInformationsController(IContactInformationService contactInformationService)
+        {
+            _contactInformationService = contactInformationService;
+        }
+
+        [HttpGet("GetByPersonId")]
+        public IActionResult GetByPersonId(int personId)
+        {
+            var result = _contactInformationService.GetListByPersonId(personId);
+
+            return Ok(result);
+        }
+    }
+}
