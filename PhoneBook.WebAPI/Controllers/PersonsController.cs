0154738f4d97b3031cd2177d23d1b75310802de8
Omer Besent <omer.besent@hotmail.com>
2023-03-17 01:01:02 +0300

    Kişi listesinin oluşturulması

---
+++ PhoneBook.WebAPI/Controllers/PersonsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Business.Abstract;
+
+namespace PhoneBook.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _personService.GetList();
+
+            return Ok(result);
+        }
+    }
+}
