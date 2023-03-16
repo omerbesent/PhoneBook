db9bd0f7ec224ddf8c194b9f75bf7e71e823e0c7
Omer Besent <omer.besent@hotmail.com>
2023-03-16 22:57:30 +0300

    Person interface ve managerının oluşturulması

---
+++ PhoneBook.Business/Abstract/IPersonService.cs
@@ -0,0 +1,12 @@
+﻿using PhoneBook.Core.Utilities.Results;
+using PhoneBook.Entities.Concrete;
+
+namespace PhoneBook.Business.Abstract
+{
+    public interface IPersonService
+    {
+        IResult Add(Person person);
+        IResult Delete(int personId);
+        IDataResult<IList<Person>> GetList();
+    }
+}
---
+++ PhoneBook.Business/Concrete/PersonManager.cs
@@ -0,0 +1,36 @@
+﻿using PhoneBook.Business.Abstract;
+using PhoneBook.Core.Utilities.Results;
+using PhoneBook.DataAccess.Abstract;
+using PhoneBook.Entities.Concrete;
+
+namespace PhoneBook.Business.Concrete
+{
+    public class PersonManager : IPersonService
+    {
+        private readonly IPersonDal _personDal;
+
+        public PersonManager(IPersonDal personDal)
+        {
+            _personDal = personDal;
+        }
+
+        public IResult Add(Person person)
+        {
+            _personDal.Add(person);
+            return new SuccessResult("Kişi oluşturuldu");
+        }
+
+        public IResult Delete(int personId)
+        {
+            var person = _personDal.Get(x => x.UUID == personId);
+            _personDal.Delete(person);
+            return new SuccessResult("Kişi silindi");
+        }
+
+        public IDataResult<IList<Person>> GetList()
+        {
+            var persons = _personDal.GetList();
+            return new SuccessDataResult<IList<Person>>(persons, "Kişiler listelendi");
+        }
+    }
+}
