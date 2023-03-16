df70ddb72d77057124a563cd5b3392ace4390e16
Omer Besent <omer.besent@hotmail.com>
2023-03-16 21:58:01 +0300

    Person data access layeri oluşturuldu

---
+++ PhoneBook.DataAccess/Concrete/EntityFramework/EfPersonDal.cs
@@ -0,0 +1,16 @@
+﻿using PhoneBook.Core.DataAccess.EntityFramework;
+using PhoneBook.DataAccess.Abstract;
+using PhoneBook.DataAccess.Concrete.EntityFramework.Contexts;
+using PhoneBook.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.DataAccess.Concrete.EntityFramework
+{
+    public class EfPersonDal : EfEntityRepositoryBase<Person, PhoneBookContext>, IPersonDal
+    {
+    }
+}
