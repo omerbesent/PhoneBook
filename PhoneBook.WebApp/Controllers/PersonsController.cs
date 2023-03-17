eef24a4f5b81966d453eb700d5c3addba65af31b
Omer Besent <omer.besent@hotmail.com>
2023-03-18 00:18:24 +0300

    PersonsController oluşturulup Layout da linkler düzenlendi

---
+++ PhoneBook.WebApp/Controllers/PersonsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneBook.WebApp.Controllers
+{
+    public class PersonsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
