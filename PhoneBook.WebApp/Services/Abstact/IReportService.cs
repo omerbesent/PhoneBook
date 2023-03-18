c15f3961a24240f67fef8e89854ea44b202e9c11
Omer Besent <omer.besent@hotmail.com>
2023-03-18 17:28:56 +0300

    ReportManager oluşturulup GetAll methodunn yazılması

---
+++ PhoneBook.WebApp/Services/Abstact/IReportService.cs
@@ -0,0 +1,11 @@
+﻿using PhoneBook.WebApp.Models.ServiceModel;
+
+namespace PhoneBook.WebApp.Services.Abstact
+{
+    public interface IReportService
+    {
+        bool RequestReport();
+        ResponseDataModel<List<Report>> GetAll();
+        byte[] ReportDownload(int reportId);
+    }
+}
