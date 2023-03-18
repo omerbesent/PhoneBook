8799d94161b3b96541168be33f8c16035e79d26e
Omer Besent <omer.besent@hotmail.com>
2023-03-18 16:56:28 +0300

    Rapor sorgusunun yazılması

---
+++ PhoneBook.Entities/Concrete/Dto/LocationReportDto.cs
@@ -0,0 +1,11 @@
+﻿using PhoneBook.Core.Entities;
+
+namespace PhoneBook.Entities.Concrete.Dto
+{
+    public class LocationReportDto : IDto
+    {
+        public string Location { get; set; }
+        public int PhoneCount { get; set; }
+        public int PersonCount { get; set; }
+    }
+}
