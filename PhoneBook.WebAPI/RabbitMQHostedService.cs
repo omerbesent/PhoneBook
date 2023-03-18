fabf0599e76fe54f6905ab047fcffb080d3634e8
Omer Besent <omer.besent@hotmail.com>
2023-03-18 17:06:08 +0300

    Raporu excel olarak oluşturup dizine kaydetmesi

--- PhoneBook.WebAPI/RabbitMQHostedService.cs
+++ PhoneBook.WebAPI/RabbitMQHostedService.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using ClosedXML.Excel;
+using Newtonsoft.Json;
 using PhoneBook.Business.Abstract;
 using PhoneBook.Entities.Concrete.Dto;
 using RabbitMQ.Client;
@@ -37,13 +38,44 @@ namespace PhoneBook.WebAPI
                 var strJson = Encoding.UTF8.GetString(e.Body.ToArray());
                 var body = JsonConvert.DeserializeObject<IList<LocationReportDto>>(strJson);
 
-                var fromReport = _reportService.GetById(Convert.ToInt32(e.BasicProperties.MessageId));
-                fromReport.Data.ReportStatus = "Tamamlandı";
-                fromReport.Data.ReportCreatedDate = DateTime.UtcNow;
-                var result = _reportService.Update(fromReport.Data);
 
+                CreateExcelReportFile(body, Convert.ToInt32(e.BasicProperties.MessageId));
                 _logger.LogInformation(strJson);
             };
+
         }
+
+        void CreateExcelReportFile(IList<LocationReportDto> report, int messageId)
+        {
+
+            using (var workbook = new XLWorkbook())
+            {
+                var ws = workbook.Worksheets.Add("Konum_Raporu");
+                ws.Range("A1").Value = "Konum";
+                ws.Range("B1").Value = "Kayıtlı Kişi Sayısı";
+                ws.Range("C1").Value = "Rehbere Kayıtlı Telefon Sayıı";
+
+                for (int i = 0; i < report.Count; i++)
+                {
+                    ws.Range($"A{i + 2}").Value = report[i].Location.ToString();
+                    ws.Range($"B{i + 2}").Value = report[i].PersonCount.ToString();
+                    ws.Range($"C{i + 2}").Value = report[i].PhoneCount.ToString();
+                }
+
+
+                var fileName = $"Konum_Raporu_{DateTime.Now.Ticks}.xlsx";
+                var filePath = $"Upload/Reports/{fileName}";
+
+                workbook.SaveAs(filePath);
+
+                var fromReport = _reportService.GetById(messageId);
+                fromReport.Data.ReportStatus = "Tamamlandı";
+                fromReport.Data.ReportPath = filePath;
+                fromReport.Data.ReportCreatedDate = DateTime.UtcNow;
+                var result = _reportService.Update(fromReport.Data);
+            }
+
+        }
+
     }
 }
