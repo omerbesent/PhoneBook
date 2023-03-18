30d14739bc330fbfdbf49f4e343d525371d78fa9
Omer Besent <omer.besent@hotmail.com>
2023-03-18 17:26:47 +0300

    report modeli oluşturuldu

---
+++ PhoneBook.WebApp/Models/ServiceModel/Report.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneBook.WebApp.Models.ServiceModel
+{
+    public class Report
+    {
+        public int UUID { get; set; }
+        public DateTime ReportCreatedDate { get; set; }
+        public string ReportStatus { get; set; }
+        public string ReportPath { get; set; }
+    }
+}
